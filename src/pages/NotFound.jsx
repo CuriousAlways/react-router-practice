ca28caebb224bed15d278e0dc570b931913310b7
raza-kh <razakhanjmi@gmail.com>
2024-08-31 21:35:01 +0530

    useNavigate uses

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
-import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => navigate('/'), 1000);
+  });
+
   return (
-    <Navigate to="/" />
+    <h1> Not Found</h1>
   )
 }
